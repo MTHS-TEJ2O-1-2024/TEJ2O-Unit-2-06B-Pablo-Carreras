4eb5aaf2d8ce22c305125eb66265036008680f37
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-27 11:06:37 -0400

    made the code

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo
+ * Created on: Sep 2024
+ * This program TURNS ON LED WITH A CHECK AND turns of with an x
 */
 
-basic.showString('Hello, World!')
+// TURNS ON LED WITH A CHECK AND turns of with an x
+
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0 )
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
